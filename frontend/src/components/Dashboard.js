c8b03ce367e994919ba953c2227dffe40e0df8a5
Tulasi krishna <Tulasikrishnabudda55@gmail.com>
2025-03-23 21:27:34 +0530

    Update the colors for the pie chart

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -58,12 +58,12 @@ const Dashboard = ({ onLogout }) => {
                     .reduce((acc, expense) => acc + parseFloat(expense.amount), 0);
             }),
             backgroundColor: [
-                'rgba(255, 99, 132, 0.6)',
-                'rgba(54, 162, 235, 0.6)',
-                'rgba(255, 206, 86, 0.6)',
-                'rgba(75, 192, 192, 0.6)',
-                'rgba(153, 102, 255, 0.6)',
-                'rgba(255, 159, 64, 0.6)'
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(255, 206, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(153, 102, 255, 0.8)',
+                'rgba(255, 159, 64, 0.8)',
+                'rgba(54, 162, 235, 0.8)'
             ],
         }],
     };
