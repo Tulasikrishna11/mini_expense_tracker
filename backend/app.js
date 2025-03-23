4c539032bace8142684521dc6e5daeff79bfc540
Tulasi krishna <Tulasikrishnabudda55@gmail.com>
2025-03-23 13:41:59 +0530

    Handle the page refresh by serving index.html for every unknown get request

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const path = require('path');
-const cors = require('cors'); // Add this line
+const cors = require('cors'); 
 const app = express();
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
 const expenseRoutes = require('./routes/expenseRoutes');
 
-app.use(cors()); // Add this line
+app.use(cors()); 
 app.use(express.json());
 
 app.use('/auth', authRoutes);
@@ -14,5 +14,9 @@ app.use('/users', userRoutes);
 app.use('/expenses', expenseRoutes);
 
 app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+});
 
 module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,6 @@ const sequelize = require('./config/sequelize');
 const PORT = process.env.PORT || 3000;
 
 sequelize.sync({force: true}).then(() => { 
-
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
     });
